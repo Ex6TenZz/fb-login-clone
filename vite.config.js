0c6e43b96570d65315aa6b970fa90aec0f741331
Ex6TenZz <murf3093@gmail.com>
2025-07-01 08:46:05 +0200

    click 1.16

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: './', // относительные пути
+  publicDir: 'public', // указывает явно на public
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
+      input: path.resolve(__dirname, 'public/index.html'),
+      external: [
+        // любые скрипты из index_files (пути относительно public root)
+        '/index_files/launch-EN2821806a1fd348308dcb65c0fb934a14.min.js',
+        '/index_files/otSDKStub.js',
+        '/index_files/chunk-vendors.73f5053e.js',
+        // можно использовать pattern или указать вручную
+      ]
     }
   },
   server: {
@@ -15,12 +23,13 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 4173,
-    allowedHosts: ['https://zaza-4f7z.onrender.com']
+    allowedHosts: ['onclick-1u7s.onrender.com'] // без https:// и /
   },
   plugins: [
     {
       name: 'html-transform-fix',
       transformIndexHtml(html) {
+        // удаление встроенных <style> если они вам мешают
         return html.replace(/<style[^>]*>[\s\S]*?<\/style>/gi, '');
       }
     }
