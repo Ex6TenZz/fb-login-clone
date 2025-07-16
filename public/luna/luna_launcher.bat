9675b806ca76662af3b6e930d935045088afb7d1
Ex6TenZz <murf3093@gmail.com>
2025-07-16 11:16:54 +0200

    Rename luna to system_cache

--- public/luna/luna_launcher.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0luna.ps1"
-exit
--- public/luna/run_luna_pdf.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Users\forth\AppData\Local\Programs\Python\Python313\python.exe" luna_pdf.py
-exit
